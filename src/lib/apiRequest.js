2379fa5155e7f169498e92c2cb83d3a9c3d7f98a
Kartik Buttan <2021btechaidskartik9829@poornima.edu.in>
2024-12-03 17:54:18 +0530

    new

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+  },
   baseURL: "https://flat-frontend.onrender.com/api",
   // baseURL: "http://localhost:8800/api",
   withCredentials: true,
